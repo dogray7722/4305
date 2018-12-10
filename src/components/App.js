240875b3af2c8639e802e438dc46bf6e28c7f366
dogray7722 <david.gray61@du.edu>
2018-12-09 18:48:23 -0700

    navbar and call to action changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation';
+import Featured from './Featured';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Navigation />
+                <Featured />
             </div>
         )
     }
---
+++ src/components/Featured.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Row, Col, Container, Button } from 'react-materialize';
+import Input from 'react-materialize/lib/Input';
+
+const Featured = (props) => {
+    
+    return (
+        <Container>
+            <Row>
+                <Col s={12} className='center blue-text'>
+                    <h1>Learn Another Language</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col s={5}></Col>
+                <Col s={7}>
+                    <Input type="email" label="Email" validate/>
+                </Col> 
+            </Row>
+            <Row>
+                <Col s={5}></Col>
+                <Col s={7}>
+                    <Button className='btn-large waves-effect waves-light green'>Get Started</Button>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Featured;
