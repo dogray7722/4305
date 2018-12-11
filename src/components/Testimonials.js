47b0ba80211ec360c6db4daed0cec59d384147fc
dogray7722 <david.gray61@du.edu>
2018-12-10 22:13:24 -0700

    add alt tags to images

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -13,7 +13,7 @@ const Testimonials = (props) => {
                 <div className="col s2 offset-s2">
                     <div className="card-panel yellow lighten-2">
                         <div className="col s8 card-image">
-                            <img src={businessman1} className="responsive-img circle"></img>
+                            <img src={businessman1} className="responsive-img circle" alt=""></img>
                         </div>
                         <div className="card-content">
                             <p>Blabberly for Business is one of the main reasons I was chosen to lead our overseas office.</p>
@@ -25,7 +25,7 @@ const Testimonials = (props) => {
                 <div className="col s2 offset-s1 push">
             <div class="card-panel light-blue darken-4">
               <div className="col s8 card-image">
-                <img src={student1} className="responsive-img circle"></img>
+                <img src={student1} className="responsive-img circle" alt=""></img>
               </div>
               <div className="card-content white-text">
                   <p>My test scores used to be very low, but after Blabberly for School, now I'm head of the class!</p>
@@ -36,7 +36,7 @@ const Testimonials = (props) => {
             <div className="col s2 offset-s1">
                 <div className="card-panel yellow lighten-2">
                   <div className="col s8 card-image">
-                    <img src={traveller1} className="responsive-img circle"></img>
+                    <img src={traveller1} className="responsive-img circle" alt=""></img>
                   </div>
                   <div className="card-content">
                     <p>Now that I can chat with the locals, my vacation experiences are so much richer!  Thanks Blabberly!</p>  
