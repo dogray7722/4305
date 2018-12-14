3467fd9729bf6b266979ff172440cc4b5ee4b5fb
dogray7722 <david.gray61@du.edu>
2018-12-13 21:51:40 -0700

    reorder links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,8 +2,11 @@ import React from 'react';
 import logo from '../images/logo.png';
 import english from '../images/uk.png';
 import spanish from '../images/spain.png';
-import {Navbar, NavItem} from 'react-materialize';
+import {Navbar} from 'react-materialize';
 import { Link } from 'react-router-dom';
+
+//Import css
+import './App.css';
 
 const Navigation = (props) => {
 
@@ -19,17 +22,17 @@ const Navigation = (props) => {
         marginBottom: '-30%'
     }
 
-    var Img = <img src={logo} style={imgStyle} alt="blabberly log"></img>
+    var Img = <img src={logo} style={imgStyle} alt="blabberly login"></img>
     var EnglishFlag = <img src={english} style={flagStyle} alt="Spanish Flag"></img>
     var SpanishFlag = <img src={spanish} style={flagStyle} alt="Union Jack"></img>
 
     return (
             
         <Navbar className = "grey lighten-1" brand={Img} right>
+            <li><Link to="/login">Login</Link></li>
             <li><Link to="/support">Support</Link></li>
             <li><Link to="/blog">Blog</Link></li>
             <li><Link to="/products">Products</Link></li>
-            <li><Link to="/login">Login</Link></li>
             <li><Link to="/">{EnglishFlag}</Link></li>
             <li><Link to="/índice">{SpanishFlag}</Link></li>
         </Navbar>
